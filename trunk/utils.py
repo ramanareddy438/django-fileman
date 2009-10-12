424d7276565faa19da15647fafef0dfa1196f164
cyxapeff <cyxapeff@1bd2d676-1540-0410-822b-41ab478d3876>
2009-10-12 17:42:48 +0000

    Unicode fixes

--- trunk/utils.py
+++ trunk/utils.py
@@ -1,11 +1,22 @@
 # -*- coding: utf-8 -*-
 
 from fileman.models import ACTIONS, History
+from fileman.settings import *
+
+def toUnicode(string):
+    if not type(string) == type(u''):
+        string = string.decode(ENCODING)
+    return string
+
+def toString(string):
+    if type(string) == type(u''):
+        string = string.encode(ENCODING)
+    return string
 
 class File:
     def __init__(self, name=None, path=None, isdir=None, size=None):
-        self.name = name
-        self.path = path
+        self.name = toUnicode(name)
+        self.path = toUnicode(path)
         self.isdir = isdir
         self.size = size
     def __cmp__(self, other):
